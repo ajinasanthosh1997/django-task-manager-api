02c62f48a0c247daae083236c696af6411a11a9b
Ajina Santhosh <139851101+ajinasanthosh1997@users.noreply.github.com>
2025-01-07 22:58:28 +0530

    Initial Commit

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TasksConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tasks'
+    name = 'main'
